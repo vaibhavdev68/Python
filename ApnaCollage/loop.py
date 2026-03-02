286318f8fc09fa791391d4e7047e95a41a61a7ba
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-02 23:14:04 +0530

    traverse

---
+++ ApnaCollage/loop.py
@@ -0,0 +1,42 @@
+# # while loop
+
+# count = 1
+# while count<=5:
+#     print('hello ')
+#     count+=1
+# print(count)           
+
+
+# i = 1
+# while i<=5:
+#     print("heloo bhai")
+#     i+=1
+# print(i)    
+
+# i = 1
+# while i<=100:
+#     print(i)
+    # i+=1
+# i = 100
+# while i>=1:
+#     print(i)
+#     i-=1
+
+
+
+# n = int(input(" Enter a number "))
+# j=1
+# while j<=10:
+#      print(f"{n} * {j} = {j*n}")
+#      j+=1
+
+
+
+
+l1 = [1,4,9,16,25,36,49, 64,81,100]
+index = 0
+while index<len(l1):
+    print(l1[index])
+    index +=1
+
+    
---
+++ QuestionPractice/question14.py
@@ -0,0 +1,4 @@
+#Write a python program to check Prime number
+
+num = int(input("Enter a number"))
+
