d3806721c15618779190545de606a12078e9be7c
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-08 19:19:29 +0530

    file handling acces and perform some operation

--- ApnaCollage/fileIo.py
+++ ApnaCollage/fileIo.py
@@ -8,84 +8,130 @@
 # open , read and close file
 # we have to open a file before reading and writing
 
-# f= open("demo.txt","r")
-# data = f.read()
-# f.close()
-# data1 = f.readline()
-# data2 = f.readline()
-# print(data)
-# print(data1)
-# print(data2)
+# # f= open("demo.txt","r")
+# # data = f.read()
+# # f.close()
+# # data1 = f.readline()
+# # data2 = f.readline()
+# # print(data)
+# # print(data1)
+# # print(data2)
 
-# f.close()
+# # f.close()
 
-# f = open("demo.txt","w")
-# f.write("i am writing in python. 7653456875")
-# f.close()
-# f = open("sample.txt","a")
+# # f = open("demo.txt","w")
+# # f.write("i am writing in python. 7653456875")
+# # f.close()
+# # f = open("sample.txt","a")
 
-# f.close()
-
-
-# f = open("sample.txt","r+")
-# f.write("abc")
-# print(f.read())
-# f.close()
-
-# f = open("sample.txt", "w+")
-# print(f.read())
-
-# f.write("hello world")
+# # f.close()
 
 
-# With Syntax 
+# # f = open("sample.txt","r+")
+# # f.write("abc")
+# # print(f.read())
+# # f.close()
+
+# # f = open("sample.txt", "w+")
+# # print(f.read())
+
+# # f.write("hello world")
 
 
-# with open("demo.txt", "r")as f:
-#     data = f.read()
-#     print(data)
-# with open("demo.txt","w") as f:
-#     data1 = f.write("new data")
-#     print(data1)
+# # With Syntax 
+
+
+# # with open("demo.txt", "r")as f:
+# #     data = f.read()
+# #     print(data)
+# # with open("demo.txt","w") as f:
+# #     data1 = f.write("new data")
+# #     print(data1)
     
 
 
-# # deleting a file 
-# import os
-# os.remove("sample.txt") 
+# # # deleting a file 
+# # import os
+# # os.remove("sample.txt") 
 
-# f = open("sample.txt", "w+")
-# f.write("hi Everyone " \
-# " we are learning File I/O" \
-# "using python." \
-# "i like programming in python"
-# )
+# # f = open("sample.txt", "w+")
+# # f.write("hi Everyone " \
+# # " we are learning File I/O" \
+# # "using python." \
+# # "i like programming in python"
+# # )
 
-# with open("practice.txt","w") as f:
-#     f.write("hi everyone\nwe are learning file i/o \n")
-#     f.write("using java.\ni like programming in java.")
-
-
-# with open("practice.txt","r") as f:
-#     data = f.read()
-# new_data = data.replace("java", "Python")
-# print(new_data)
-# with open("practice.txt","w") as f:
-#     f.write(new_data)
+# # with open("practice.txt","w") as f:
+# #     f.write("hi everyone\nwe are learning file i/o \n")
+# #     f.write("using java.\ni like programming in java.")
 
 
-#Try to find "learning" in practice.txt
-
-# with open("practice.txt","r") as f:
-#     data = f.read()
-#     if(data.find("learning") >=0):
-#         print("found")
-#     else:
-#         print("not found")
+# # with open("practice.txt","r") as f:
+# #     data = f.read()
+# # new_data = data.replace("java", "Python")
+# # print(new_data)
+# # with open("practice.txt","w") as f:
+# #     f.write(new_data)
 
 
+# #Try to find "learning" in practice.txt
 
-# Waf to find in which line of the file does the word "learning" occur first
-# print -1 if word not found
+# # with open("practice.txt","r") as f:
+# #     data = f.read()
+# #     if(data.find("learning") >=0):
+# #         print("found")
+# #     else:
+# #         print("not found")
+
+
+
+# # Waf to find in which line of the file does the word "learning" occur first
+# # print -1 if word not found
+
+# def check_word(word):
+#     with open("practice.txt", "r") as f:
+#         data = f.read()
+#         if(data.find(word) !=-1):
+#             print("found")
+#         else:
+#             print("not found")
+
+# def check_line(word):
+#     data = True
+#     line_no = 1
+#     with open("practice.txt","r") as f:
+#         while data:
+#             data = f.readline()
+#             if(word in data):
+#                 print(line_no)
+#                 return
+#             line_no +=1
+#     return -1
+# check_line("Python")
+count = 0
+
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+
+    num = data.split(",")
+    print(num)
+
+    for val in num:
+        if(int(val)%2 ==0):
+            print(val, "even")
+            count+=1
+    print(count)
+    
+       
+                                                                   # num =""
+                                                                   # for i in range (len(data)):
+                                                                   #     if(data[i] ==","):
+                                                                   #         print(int(num))
+                                                                   #         num=""
+                                                                   #     else:
+                                                                   #         num+=data[i]
+
+    
 
 
