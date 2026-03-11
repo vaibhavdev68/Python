c44e1b6ffef408e60344e846149dc00392aca39c
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-11 23:13:32 +0530

    oops started

--- ApnaCollage/fileIo.py
+++ ApnaCollage/fileIo.py
@@ -277,3 +277,17 @@
 # open(): Opens the file in read mode.
 # read(): Reads the content of the file.
 # finally: Ensures the code inside it runs no matter what.
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+# Creating object
+s1 = Student("Rahul", 20)
+s1.display()
