14583de5926f71a0a45cc91f776c9bb1adb500a6
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-16 23:34:55 +0530

    day 3 of learning oops

--- ApnaCollage/oops.py
+++ ApnaCollage/oops.py
@@ -1,22 +1,46 @@
-def under_attack(col, queens):
-    left = right = col
+# def under_attack(col, queens):
+#     left = right = col
 
-    for r, c in reversed(queens):
-        left, right = left - 1, right + 1
+#     for r, c in reversed(queens):
+#         left, right = left - 1, right + 1
 
-        if c in (left, col, right):
-            return True
-    return False
+#         if c in (left, col, right):
+#             return True
+#     return False
 
-def solve(n):
-    if n == 0:
-        return [[]]
+# def solve(n):
+#     if n == 0:
+#         return [[]]
 
-    smaller_solutions = solve(n - 1)
+#     smaller_solutions = solve(n - 1)
 
-    return [solution+[(n,i+1)]
-        for i in range(BOARD_SIZE)
-            for solution in smaller_solutions
-                if not under_attack(i+1, solution)]
-for answer in solve(BOARD_SIZE):
-    print (answer)
+#     return [solution+[(n,i+1)]
+#         for i in range(BOARD_SIZE)
+#             for solution in smaller_solutions
+#                 if not under_attack(i+1, solution)]
+# for answer in solve(BOARD_SIZE):
+#     print (answer)
+
+
+
+
+
+class Parrot:
+
+    # class attribute
+    name = ""
+    age = 0
+
+# create parrot1 object
+parrot1 = Parrot()
+parrot1.name = "Blu"
+parrot1.age = 10
+
+# create another object parrot2
+parrot2 = Parrot()
+parrot2.name = "Woo"
+parrot2.age = 15
+
+# access attributes
+print(f"{parrot1.name} is {parrot1.age} years old")
+print(f"{parrot2.name} is {parrot2.age} years old")
