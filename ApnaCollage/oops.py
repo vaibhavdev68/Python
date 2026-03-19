f7f8742ce57f4a89e924b2e192b651af1efc6e94
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-19 09:47:38 +0530

    day 3 of learning oops

--- ApnaCollage/oops.py
+++ ApnaCollage/oops.py
@@ -1,57 +1,55 @@
-# # def under_attack(col, queens):
-# #     left = right = col
+# # # def under_attack(col, queens):
+# # #     left = right = col
 
-# #     for r, c in reversed(queens):
-# #         left, right = left - 1, right + 1
+# # #     for r, c in reversed(queens):
+# # #         left, right = left - 1, right + 1
 
-# #         if c in (left, col, right):
-# #             return True
-# #     return False
+# # #         if c in (left, col, right):
+# # #             return True
+# # #     return False
 
-# # def solve(n):
-# #     if n == 0:
-# #         return [[]]
+# # # def solve(n):
+# # #     if n == 0:
+# # #         return [[]]
 
-# #     smaller_solutions = solve(n - 1)
+# # #     smaller_solutions = solve(n - 1)
 
-# #     return [solution+[(n,i+1)]
-# #         for i in range(BOARD_SIZE)
-# #             for solution in smaller_solutions
-# #                 if not under_attack(i+1, solution)]
-# # for answer in solve(BOARD_SIZE):
-# #     print (answer)
-
-
+# # #     return [solution+[(n,i+1)]
+# # #         for i in range(BOARD_SIZE)
+# # #             for solution in smaller_solutions
+# # #                 if not under_attack(i+1, solution)]
+# # # for answer in solve(BOARD_SIZE):
+# # #     print (answer)
 
 
 
-# class Parrot:
-
-#     # class attribute
-#     name = ""
-#     age = 0
-
-# # create parrot1 object
-# parrot1 = Parrot()
-# parrot1.name = "Blu"
-# parrot1.age = 10
-
-# # create another object parrot2
-# parrot2 = Parrot()
-# parrot2.name = "Woo"
-# parrot2.age = 15
-
-# # access attributes
-# print(f"{parrot1.name} is {parrot1.age} years old")
-# print(f"{parrot2.name} is {parrot2.age} years old")
 
 
-# start, end = -5, 0  
-# for i in range(start, end + 1):  
-#     if i < 0:  # Check if the number is negative
-#         print(i)
+# # class Parrot:
+
+# #     # class attribute
+# #     name = ""
+# #     age = 0
+
+# # # create parrot1 object
+# # parrot1 = Parrot()
+# # parrot1.name = "Blu"
+# # parrot1.age = 10
+
+# # # create another object parrot2
+# # parrot2 = Parrot()
+# # parrot2.name = "Woo"
+# # parrot2.age = 15
+
+# # # access attributes
+# # print(f"{parrot1.name} is {parrot1.age} years old")
+# # print(f"{parrot2.name} is {parrot2.age} years old")
 
 
+# # start, end = -5, 0  
+# # for i in range(start, end + 1):  
+# #     if i < 0:  # Check if the number is negative
+# #         print(i)
 
 
 
@@ -64,6 +62,27 @@
 
 
 
-x, y = input("Enter two values: ").split()
-print("Number of boys: ", x)
-print("Number of girls: ", y)
+
+
+# x, y = input("Enter two values: ").split()
+# print("Number of boys: ", x)
+# print("Number of girls: ", y)
+
+
+
+
+def greet(name: str, times: int = 1) -> str:
+    """
+    Generate a greeting message.
+
+    Args:
+        name: The person's name.
+        times: How many times to repeat the greeting.
+
+    Returns:
+        The greeting string.
+    """
+    return f"Hello, {name}! " * times
+
+hi = greet("vaibahv")
+print(hi)
