b0132cacec6aae2b18210f2f89f191eba689e785
Vaibhav verma <vaibhavverma91201@gmail.com>
2026-03-25 23:39:25 +0530

    less practice due to health issues

--- Logic Building/ForLoop/q21.py
+++ Logic Building/ForLoop/q21.py
@@ -6,3 +6,14 @@ for i in range(1, n+1):
     if i % 2 != 0:
         sum+=i
 print(sum)
+
+
+
+
+
+n = int(input("Enter a number : "))
+sum = 0
+for i in range(1, n+1):
+    if i % 2 != 0:
+        sum+=i
+print(sum)
